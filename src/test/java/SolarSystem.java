1b7518ecf810e63c2c97cfbf412440df375169eb
TristanF-00 <tfsaypharath@gmail.com>
2025-06-12 22:01:04 -0400

    solar system basic structure is ready

---
+++ src/test/java/SolarSystem.java
@@ -0,0 +1,10 @@
+public class SolarSystem {
+    public static void main(String[] args) {
+        BasicStructure BB = new BasicStructure();
+        BB.Planets = "2";
+        BB.Moon = "23";
+        BB.Stars = "43";
+        BB.Sun = "5";
+        System.out.println("solar system is ready");
+    }
+}
